88002c00849769b35e057e76bcb1c4836076dbfe
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 14:35:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function highlight() {
     //Write your code here
-
+const bolword=document.getElementByTagName("strong");
+	for(let i=0 ; i<bolword.length ; i++)
+		{
+			bolword.style.color="green";
+		}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const bolword=document.getElementByTagName("strong");
+	for(let i=0 ; i<bolword.length ; i++)
+		{
+			bolword.style.color="black";
+		}
     
 }
